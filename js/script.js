6c17fe58e5140e17438f91413262626e157e8abe
Olena Lytvynenko <lena.litvinen4ik@gmail.com>
2025-12-11 16:08:45 +0200

    wave

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// const feedbackList = document.querySelector('.feedback-list');
+
+// const feedbackImgToHtml = feedbackImages.map(
+//     (image) => 
+//         `
+//         <li class="feedback-item"
+//         <img 
+//             src="${image.src1x}"
+//             srcset="
+//                 ${image.src1x} 1x,
+//                 ${image.src2x} 2x
+//             "
+//             alt="${image.alt}"
+//             class="feedback-img"
+//             />
+//         </li>
+//         `
+// )
+// .join('');
+
+// feedbackList.innerHTML = feedbackImgToHtml;
+
+//https://www.instagram.com/artlover_lessons?utm_source=qr&igsh=MXFrcGF6MHBsajU4OQ==
+
+    // document.querySelector('#btn-instagram').addEventListener('click', function (event) {
+    //     event.preventDefault();
+    //     window.location.href = 'https://www.instagram.com/artlover_lessons?utm_source=qr&igsh=MXFrcGF6MHBsajU4OQ==';
+    // });
